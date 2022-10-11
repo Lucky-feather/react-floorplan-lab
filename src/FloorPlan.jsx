f9fa9e29f9f3bfcd70084866ca37858e793dae76
LH <lindsaynhenderson@gmail.com>
2022-10-10 19:07:31 -0500

    add bathrooms

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,17 +1,20 @@
-
+import Bath from "./Bath"
 import Bedroom from "./Bedroom"
 import Kitchen from "./Kitchen"
 import LivingRoom from "./LivingRoom"
+
 function FloorPlan(props) {
   return (
     <div>
       <Bedroom bedNum = {1}/>
+      <Bath  size = {'Half'}/>
       <Kitchen />
       <LivingRoom />
       <Bedroom bedNum = {2}/>
+      <Bath size = {'Full'} />
       <Bedroom bedNum = {3}/>
 
-    <h3>Bath</h3>
+
     </div>
   )
 }
